aacc376d41e3428733cc56d652f942aa6edbb27f
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-09-17 22:41:20 -0700

    admin

--- blog_application/settings.py
+++ blog_application/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-fr^he5szh8s%tk(x#4!_pek5o!a$t2_np$p(42#=u4n#q=rvxu"
@@ -97,6 +98,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
