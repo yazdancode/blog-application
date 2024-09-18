dae02580971f7bbf359d9d0ed93c1294e563c546
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-09-17 21:47:06 -0700

    views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
-# Create your views here.
+def detail(request, slug):
+	post = get_object_or_404(Post, slug=slug)
+	
